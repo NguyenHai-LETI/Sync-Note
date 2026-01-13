0e35cfa3ab9324a4caf8e4bbbcdac5e8deb15756
Nguyen Hai <hai.nguyen12@ntq-solution.com.vn>
2026-01-13 12:30:41 +0700

    fix: change duration of access token

--- config/settings.py
+++ config/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT Configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
